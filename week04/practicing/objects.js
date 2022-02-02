4c7f6c16a41483828afff5ffd01ae8c5c5ca17aa
Agustin Aguilar <aguilarag98@gmail.com>
2022-02-02 16:15:30 -0300

    practicing objects

--- week04/practicing/objects.js
+++ week04/practicing/objects.js
@@ -2,4 +2,26 @@
 const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
+    sections:[{ sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}]
   };
+
+  function nameNumber(course){
+      document.getElementById('courseName').innerText=course.name
+      document.getElementById('courseCode').innerText=course.code
+  }
+  function sect(sections){
+      const html = sections.map(
+    (section) => `<tr>
+    <td>${section.sectionNum}</td>
+    <td>${section.roomNum}</td>
+    <td>${section.enrolled}</td>
+    <td>${section.days}</td>
+    <td>${section.instructor}</td></tr>`
+  );
+    document.querySelector('#sections').innerHTML=html.join('')
+  }
+
+
+  nameNumber(aCourse)
+  sect(aCourse.sections);
