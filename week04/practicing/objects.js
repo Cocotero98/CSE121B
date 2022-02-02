ecccdd9bac14985e6ab902d3132f83be66c46186
Agustin Aguilar <aguilarag98@gmail.com>
2022-02-02 19:24:00 -0300

    practice finsihed

--- week04/practicing/objects.js
+++ week04/practicing/objects.js
@@ -4,22 +4,20 @@ const aCourse = {
     name: "Javascript Language",
     sections:[{ sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
     { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}],
-    enrollStudent: function(sectionNum){
-        const index=this.sections.findIndex((section)=>section.sectionNum==sectionNum)//encuentra el indice donde esto ssea verdad?
-        if (index>=0){
-            this.sections[index].enrolled++;
-            sect(this.sections);
-        }
-    },
-    dropStudent: function(sectionNum){
+    enrollOrDrop: function(sectionNum, moreOrLess){
         const index=this.sections.findIndex((section)=>section.sectionNum==sectionNum)
         if (index>=0){
-            this.sections[index].enrolled--;
+            if (moreOrLess==='less'){
+                this.sections[index].enrolled--;
+            }
+            else if (moreOrLess==='more'){
+                this.sections[index].enrolled++;
+            }
             sect(this.sections);
+            };
         }
     }
-
-  };
+     
 
   function nameNumber(course){
       document.getElementById('courseName').innerText=course.name
@@ -37,15 +35,15 @@ const aCourse = {
     document.querySelector('#sections').innerHTML=html.join('')
   }
 
-document.querySelector("#enrollStudent").addEventListener('click', function (){
+    document.querySelector("#enrollStudent").addEventListener('click', function (){
     const sectionNumb=document.querySelector('#sectionNumber').value;
-    aCourse.enrollStudent(sectionNumb);
-})
-document.querySelector('#dropStudent').addEventListener('click', function(){
+    aCourse.enrollOrDrop(sectionNumb, 'more');
+    })
+    document.querySelector('#dropStudent').addEventListener('click', function(){
     const sectionNumb=document.querySelector('#sectionNumber').value;
-    aCourse.dropStudent(sectionNumb);
-})
+    aCourse.enrollOrDrop(sectionNumb, 'less');
+    })
 
 
   nameNumber(aCourse)
-  sect(aCourse.sections);
+  sect(aCourse.sections)
