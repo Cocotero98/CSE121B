c0dc94a751176b73f864e02cf6cad1c1c5517732
Agustin Aguilar <aguilarag98@gmail.com>
2022-02-02 18:48:22 -0300

    finish practice

--- week04/practicing/objects.js
+++ week04/practicing/objects.js
@@ -3,7 +3,22 @@ const aCourse = {
     code: "CSE121b",
     name: "Javascript Language",
     sections:[{ sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
-    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}]
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}],
+    enrollStudent: function(sectionNum){
+        const index=this.sections.findIndex((section)=>section.sectionNum==sectionNum)//encuentra el indice donde esto ssea verdad?
+        if (index>=0){
+            this.sections[index].enrolled++;
+            sect(this.sections);
+        }
+    },
+    dropStudent: function(sectionNum){
+        const index=this.sections.findIndex((section)=>section.sectionNum==sectionNum)
+        if (index>=0){
+            this.sections[index].enrolled--;
+            sect(this.sections);
+        }
+    }
+
   };
 
   function nameNumber(course){
@@ -21,6 +36,15 @@ const aCourse = {
   );
     document.querySelector('#sections').innerHTML=html.join('')
   }
+
+document.querySelector("#enrollStudent").addEventListener('click', function (){
+    const sectionNumb=document.querySelector('#sectionNumber').value;
+    aCourse.enrollStudent(sectionNumb);
+})
+document.querySelector('#dropStudent').addEventListener('click', function(){
+    const sectionNumb=document.querySelector('#sectionNumber').value;
+    aCourse.dropStudent(sectionNumb);
+})
 
 
   nameNumber(aCourse)
