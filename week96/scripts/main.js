b208588ff7df5534258d477115febe9cb4635d31
Cocotero98 <aguilarag98@gmail.com>
2022-02-16 15:07:50 -0300

    .

--- week96/scripts/main.js
+++ week96/scripts/main.js
@@ -1,3 +1,53 @@
+// date
+const date= new Date()
+const year= date.getFullYear()
+const month=date.getMonth()
+const day=date.getDate()
+const currentDate=`${year}/${month}/${day}`
+
+document.getElementById('day').innerHTML=currentDate;
+// toggle menu to open a list in html (Workout, Sleep, Food, Study). Similar to the temple ascending/descending
+
+//add a space to insert the date (key), add a space to insert what you did (value)
+
+//Add a button to display the stored information
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 const url = "https://pokeapi.co/api/v2/pokemon";
 let results = null;
 // takes a fetch response and checks to make sure it was OK.
