403cd1a82cbc270e9e61a09b2801ab082fa9cea9
Cocotero98 <aguilarag98@gmail.com>
2022-02-16 22:40:46 -0300

    Task6 finished

--- week96/scripts/main.js
+++ week96/scripts/main.js
@@ -4,7 +4,7 @@ const year= date.getFullYear()
 const month=date.getMonth()
 const day=date.getDate()
 const currentDate=`${year}/${month}/${day}`
-
+document.getElementById('year').innerHTML=year
 document.getElementById('day').innerHTML=currentDate;
 
 let store= function(){
@@ -35,8 +35,6 @@ button.addEventListener('click',store)
 let show=document.getElementById('show')
 show.addEventListener('click', showMe)
 
-// button.addEventListener('click', console.log('works'))
-// toggle menu to open a list in html (Workout, Sleep, Food, Study). Similar to the temple ascending/descending
 
 const snorlax='https://pokeapi.co/api/v2/pokemon/snorlax'
 const jigglypuff='https://pokeapi.co/api/v2/pokemon/jigglypuff'
@@ -45,89 +43,33 @@ const machoke='https://pokeapi.co/api/v2/pokemon/machoke'
 
 
 let url=''
-switch(activity.value){
-  case 'Workout':
-    url=machoke;
-    break;
-  case 'Sleep':
-    url=jigglypuff;
-    break;
-    case 'Food':
-    url=snorlax;
-    break;
-  case 'Study':
-    url=mewtwo;
-    break;
-}
-function addImg(data){
-  const pokemonImg=document.getElementById('img')
-  results=data
-  dreamWorld=results.dream_world
-  // console.log("first: ", results.sprites.front_default);
-  let img= document.createElement('img')
-  img.setAttribute('src',results.sprites.front_default)
-  img.setAttribute('alt',results.name)
-  pokemonImg.appendChild(img)
-}
+document.getElementById('img').innerHTML=`<h3>Workout like Machoke, eat like Snorlax, study like MewTwo, Sleep like Jigglypuff</h3>`
 
-fetch(url)
+
+  url=machoke
+  fetch(url)
+  .then((response)=>{return response.json()})
+  .then(addImg)
+
+    url=jigglypuff
+    fetch(url)
 .then((response)=>{return response.json()})
 .then(addImg)
 
-//add a space to insert the date (key), add a space to insert what you did (value)
+  url=snorlax
+  fetch(url)
+.then((response)=>{return response.json()})
+.then(addImg)
 
-//Add a button to display the stored information
+  url=mewtwo
+  fetch(url)
+.then((response)=>{return response.json()})
+.then(addImg)
 
-//
-
-
-
-
-
-
-
-
-
-
-
-
+function addImg(data){
+  let pokemonImg=document.getElementById('img')
+  results=data
+  pokemonImg.innerHTML+= `<img src=${results.sprites.front_default} alt=${results.name}>`
+}
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const url = "https://pokeapi.co/api/v2/pokemon";
-// let results = null;
-// // takes a fetch response and checks to make sure it was OK.
-// // then returns the body of the response as a PROMISE to some JSON.
-// function convertToJson(response) {
-//   if (response.ok) {
-//     return response.json();
-//   } else {
-//     console.log("error:", response);
-//   }
-// }
-// function doStuff(data){
-//     results=data
-//     console.log('first',results)
-// }
-
-// fetch(url).then(convertToJson).then(doStuff)
